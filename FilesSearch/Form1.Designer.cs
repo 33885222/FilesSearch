2a716709b614764467ee50f87eb24c7c47b1ae4c
33885222 <33885222@mail.ru>
2014-06-08 14:13:46 +0400

    1

---
+++ FilesSearch/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace FilesSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.startFolderTxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fileNameTxt = new System.Windows.Forms.TextBox();
+            this.textTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.processedFilesLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timerLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.currentFileNameLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Стартовая директория:";
+            // 
+            // startFolderTxt
+            // 
+            this.startFolderTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startFolderTxt.Location = new System.Drawing.Point(143, 6);
+            this.startFolderTxt.Name = "startFolderTxt";
+            this.startFolderTxt.Size = new System.Drawing.Size(215, 20);
+            this.startFolderTxt.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(364, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Обзор...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(12, 113);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(427, 242);
+            this.treeView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Поиск:";
+            // 
+            // fileNameTxt
+            // 
+            this.fileNameTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileNameTxt.Location = new System.Drawing.Point(143, 32);
+            this.fileNameTxt.Name = "fileNameTxt";
+            this.fileNameTxt.Size = new System.Drawing.Size(215, 20);
+            this.fileNameTxt.TabIndex = 5;
+            // 
+            // textTxt
+            // 
+            this.textTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textTxt.Location = new System.Drawing.Point(143, 58);
+            this.textTxt.Name = "textTxt";
+            this.textTxt.Size = new System.Drawing.Size(215, 20);
+            this.textTxt.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Текст в файле:";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(180, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Tag = "0";
+            this.button2.Text = "Искать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.processedFilesLbl,
+            this.toolStripStatusLabel2,
+            this.timerLbl,
+            this.toolStripStatusLabel4,
+            this.currentFileNameLbl});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 358);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(450, 22);
+            this.statusStrip1.TabIndex = 12;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(155, 17);
+            this.toolStripStatusLabel1.Text = "Всего обработано файлов:";
+            // 
+            // processedFilesLbl
+            // 
+            this.processedFilesLbl.Name = "processedFilesLbl";
+            this.processedFilesLbl.Size = new System.Drawing.Size(13, 17);
+            this.processedFilesLbl.Text = "0";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(18, 17);
+            this.toolStripStatusLabel2.Text = "за";
+            // 
+            // timerLbl
+            // 
+            this.timerLbl.Name = "timerLbl";
+            this.timerLbl.Size = new System.Drawing.Size(13, 17);
+            this.timerLbl.Text = "0";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(25, 17);
+            this.toolStripStatusLabel4.Text = "сек";
+            // 
+            // currentFileNameLbl
+            // 
+            this.currentFileNameLbl.Name = "currentFileNameLbl";
+            this.currentFileNameLbl.Size = new System.Drawing.Size(15, 17);
+            this.currentFileNameLbl.Text = "()";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 380);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textTxt);
+            this.Controls.Add(this.fileNameTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.startFolderTxt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Поиск файлов";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox startFolderTxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fileNameTxt;
+        private System.Windows.Forms.TextBox textTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel processedFilesLbl;
+        private System.Windows.Forms.ToolStripStatusLabel currentFileNameLbl;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel timerLbl;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+    }
+}
+
---
+++ FilesSearch/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Xml;
+
+namespace FilesSearch
+{
+    public partial class Form1 : Form
+    {
+        DirectoryInfo path;
+        TreeNode tn;
+        bool working;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadConfig();
+        }
+
+        void SaveConfig()
+        {
+            XmlTextWriter writer = null;
+            writer = new XmlTextWriter("config.xml", System.Text.Encoding.Unicode);
+            writer.WriteStartDocument();
+            writer.WriteStartElement("Настройки");
+            writer.WriteAttributeString("Директория", startFolderTxt.Text);
+            writer.WriteAttributeString("ИмяФайла", fileNameTxt.Text);
+            writer.WriteAttributeString("Текст", textTxt.Text);
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Close();
+        }
+
+        void LoadConfig()
+        {
+            XmlTextReader reader = null;
+            try
+            {
+                reader = new XmlTextReader("config.xml");
+                reader.WhitespaceHandling = WhitespaceHandling.None;
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        if (reader.Name == "Настройки")
+                        {
+                            startFolderTxt.Text = reader.GetAttribute("Директория");
+                            fileNameTxt.Text = reader.GetAttribute("ИмяФайла");
+                            textTxt.Text = reader.GetAttribute("Текст");
+                        }
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                SaveConfig();
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog Folder = new FolderBrowserDialog();
+            if (Folder.ShowDialog() == DialogResult.OK)
+                startFolderTxt.Text = Folder.SelectedPath;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread th;
+            th = new Thread(StartSearch);
+            if (button2.Text == "Искать")
+            {
+                treeView1.Nodes.Clear();
+                processedFilesLbl.Text = "0";
+                timerLbl.Text = "0";
+                currentFileNameLbl.Text = "()";
+                path = new DirectoryInfo(startFolderTxt.Text);
+                tn = treeView1.Nodes.Add(startFolderTxt.Text);
+                working = true;
+                th.Start();
+                button2.Text = "Остановить";
+            }
+            else
+            {
+                working = false;
+                if (th != null)
+                    th.Abort();
+                button2.Text = "Искать";
+            }
+        }
+
+        void StartSearch()
+        {
+            new Thread(Timer).Start();
+            FindFiles(path, fileNameTxt.Text, textTxt.Text, tn);
+            working = false;
+            Invoke(new MethodInvoker(delegate
+            {
+                button2.Text = "Искать";
+            }));
+        }
+
+        void Timer()
+        {
+            while (working)
+            {
+                Invoke(new MethodInvoker(delegate
+                {
+                    timerLbl.Text = (Convert.ToInt32(timerLbl.Text) + 1).ToString();
+                }));
+                Thread.Sleep(1000);
+            }
+        }
+
+        void FindFiles(DirectoryInfo path, string filename, string filetext, TreeNode treenode)
+        {
+            try
+            {
+                foreach (DirectoryInfo dir in path.GetDirectories())
+                {
+                    if (!working)
+                        return;
+                    UpdateCurrentFileName(dir.Name);
+                    FindFiles(dir, filename, filetext, AddNode(treenode, dir.Name));
+                }
+                foreach (FileInfo f in path.GetFiles(filename))
+                {
+                    if (!working)
+                        return;
+                    UpdateCurrentFileName(f.Name);
+                    UpdateFilesCount();
+                    if (ReadFile(f.FullName, filetext))
+                        AddNode(treenode, f.Name);
+                }
+                if (treenode.Nodes.Count == 0)
+                    RemoveNode(treenode);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                RemoveNode(treenode);
+            }
+        }
+
+        TreeNode AddNode(TreeNode treenode, string treenodename)
+        {
+            TreeNode tn = new TreeNode();
+            Invoke(new MethodInvoker(delegate
+            {
+                tn = treenode.Nodes.Add(treenodename);
+            }));
+            return tn;
+        }
+
+        void RemoveNode(TreeNode treenode)
+        {
+            Invoke(new MethodInvoker(delegate
+            {
+                treenode.Remove();
+            }));
+        }
+
+        void UpdateFilesCount()
+        {
+            processedFilesLbl.Text = (Convert.ToInt32(processedFilesLbl.Text) + 1).ToString();
+        }
+
+        void UpdateCurrentFileName(string filename)
+        {
+            currentFileNameLbl.Text = string.Format("({0})", filename);
+        }
+
+        bool ReadFile(string filename, string filetext)
+        {
+            string line = "";
+            try
+            {
+                FileStream f = File.OpenRead(filename);
+                StreamReader fs = new StreamReader(f, System.Text.Encoding.GetEncoding(1251));
+                while ((line = fs.ReadLine()) != null)
+                {
+                    //MessageBox.Show(line);
+                    if (line.Contains(filetext))
+                    {
+                        f.Close();
+                        return true;
+                    }
+                }
+                f.Close();
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveConfig();
+        }
+    }
+}
